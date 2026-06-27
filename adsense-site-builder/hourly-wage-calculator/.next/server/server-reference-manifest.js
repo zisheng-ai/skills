c7bba71f348b90dab86d9c73e58a3ab08be23eca
紫升 <yangjunning.yjn@alibaba-inc.com>
2026-06-27 23:45:09 +0800

    Remove hourly wage calculator files and configuration from AdSense Site Builder

--- adsense-site-builder/hourly-wage-calculator/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dQ2Le0+dzMkCPBVG+awtowjX2WzTE0UhHok73DNA/1I=\"\n}"
--- adsense-site-builder/hourly-wage-calculator/.next/static/chunks/src_app_layout_tsx_a80ab245._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9a168d68._.css",
-  "static/chunks/f83a4_next_dist_7b2d1b97._.js"
-],
-    source: "dynamic"
-});
